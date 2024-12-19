b218994e77fae38273a3bdd810be7e0ee0f055fd
ObiFlo1 <rubflores1000@gmail.com>
2024-12-19 14:32:25 -0800

    update form sign-in page

--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -7,29 +7,25 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Divide } from "lucide-react";
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
+import CustomInput from "./CustomInput";
+import { authFormSchema } from "@/lib/utils";
 
 const AuthForm = ({ type }: { type: string }) => {
   const [user, setUser] = useState(null);
 
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof authFormSchema>>({
+    resolver: zodResolver(authFormSchema),
     defaultValues: {
-      username: "",
+      email: "",
+      password: "",
     },
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof authFormSchema>) {
     console.log(values);
   }
 
@@ -68,20 +64,19 @@ const AuthForm = ({ type }: { type: string }) => {
               onSubmit={form.handleSubmit(onSubmit)}
               className="space-y-8"
             >
-              <FormField
+              <CustomInput
                 control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <div className="form-item">
-                    <FormLabel className="form-label">Email</FormLabel>
-                    <div className="flex w-full flex-col">
-                      <FormControl>
-                        <Input placeholder="Enter your email" />
-                      </FormControl>
-                    </div>
-                  </div>
-                )}
+                name="username"
+                label="Username"
+                placeholder="Enter your username"
               />
+              <CustomInput
+                control={form.control}
+                name="password"
+                label="Password"
+                placeholder="Enter your password"
+              />
+
               <Button type="submit">Submit</Button>
             </form>
           </Form>
---
+++ components/ui/CustomInput.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { FormControl, FormField, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control, Form } from "react-hook-form";
+
+interface CustomInput {
+  control: Control;
+  name: string;
+  label: string;
+  placeholder: string;
+}
+
+const CustomInput = ({ control, name, label, placeholder }: CustomInput) => {
+  return (
+    <>
+      <FormField
+        control={control}
+        name={name}
+        render={({ field }) => (
+          <div className="form-item">
+            <FormLabel className="form-label">{label}</FormLabel>
+            <div className="flex w-full flex-col">
+              <FormControl>
+                <Input
+                  placeholder={placeholder}
+                  className="input-class"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="form-message mt-2" />
+            </div>
+          </div>
+        )}
+      />
+    </>
+  );
+};
+
+export default CustomInput;
