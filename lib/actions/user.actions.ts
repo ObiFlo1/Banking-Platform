0dd51dccacc542c85a4aa505d09c2cb17e686c65
ObiFlo1 <rubflores1000@gmail.com>
2024-12-25 18:05:48 -0800

    make new folder w/ user server actions

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,9 @@
+"use server";
+
+export const signIn = async () => {
+  try {
+    //what we can do here: Mutation/Database/Make fetch request/
+  } catch (error) {
+    console.error("error");
+  }
+};
